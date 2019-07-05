2b8b9e22931feeb0a472787255122dc75d945340
Kyle Johnston <Kyle.Johnston@Verint.com>
2019-07-05 16:35:14 +0100

    Added basic form with comments explaining what everything is doing

---
+++ src/components/GameForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+class GameForm extends Component {
+    constructor(props) {
+        super(props)
+        //This creates the component state object that we will post to our DB etc
+        this.state = {
+            player1: '',
+            player2: '',
+            p1Score: '',
+            p2Score: ''
+        }
+        // We have to bind functions so that they can be used
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // This takes a change event on our input field and then finds the relevant key in the state and sets its value to the value of the input field
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // This handles the button click 
+    handleSubmit(event) {
+        // This stops the defualt action of buttons which is to post to the browser URL
+        event.preventDefault();
+
+        // Just checking that our function is sending data
+        console.log("This is my post", this.state);
+
+        // This is a function propped down from our main container we pass it the state from our form and it handles adding it to the games array in our main state
+        this.props.newGame(this.state)
+
+        // Our input fields get their default value from the state so we set state back to empty strings after everything else to clear the form fields
+        this.setState({
+            player1: '',
+            player2: '',
+            p1Score: '',
+            p2Score: ''
+        })
+    }
+
+    // You cannot comment inside the render as anything in there is rendered on the page
+    // 
+    // The onchange in the input calls the handleChange function whenever a change happens in the field
+    // The onClick in the button calls the handleSubmit function when we click the save button in browser
+    render() {
+        return (
+            <>
+                <p>Add new Game!</p>
+                <form>
+                    <div>
+                        <label htmlFor="player1">Player 1</label>
+                        <input type='text' name='player1' onChange={this.handleChange} value={this.state.player1} />
+                    </div>
+                    <div>
+                        <label htmlFor="p1Score">Player 1 Score</label>
+                        <input type='number' name='p1Score' onChange={this.handleChange} value={this.state.p1Score} />
+                    </div>
+                    <div>
+                        <label htmlFor="player2">Player 2</label>
+                        <input type='text' name='player2' onChange={this.handleChange} value={this.state.player2} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="p2Score">Player 2 Score</label>
+                        <input type='number' name='p2Score' onChange={this.handleChange} value={this.state.p2Score} />
+                    </div>
+                    <div>
+                        <button onClick={this.handleSubmit}>Save</button>
+                    </div>
+                </form>
+            </>
+        )
+    }
+}
+
+// You need to export your components etc so theyt can be read by other pages
+export default GameForm;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,28 +1,37 @@
 import React, {Component} from 'react';
-import SimpleComponent from '../components/SimpleComponent';
+import GameForm from '../components/GameForm';
 
 class MainContainer extends Component{
     constructor(props){
         super(props)
         this.state = {
-            counter: 0
+            // counter: 0,
+            games: []
         }
-        this.counterAdder = this.counterAdder.bind(this);
+       // this.counterAdder = this.counterAdder.bind(this);
+       this.addNewGame = this.addNewGame.bind(this); 
     }
 
-     counterAdder() {
-        
-        let prevState = this.state.counter;
-        prevState += 1;
+    //  counterAdder() {
+    //     let prevState = this.state.counter;
+    //     prevState += 100;
+    //     let newState = prevState;
+    //     this.setState({counter: newState})
+    // }
+
+    // This function will add the new game from our game form to the games array in our main containers state
+    addNewGame(game){
+        let prevState = this.state.games;
+        prevState.push(game);
         let newState = prevState;
-        this.setState({counter: newState})
+        this.setState({counter: newState});
     }
 
     render(){
         return(
             <>
                 <h2>Hello from MainContainerTest</h2>
-                <SimpleComponent count = {this.state.counter} addMore = {this.counterAdder}/>
+                <GameForm newGame = {this.addNewGame}/>
             </>
         )
     }
